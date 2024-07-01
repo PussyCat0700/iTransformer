9adb4b71663d32fae10457a1983b6a75b91fcb38
1729372667@qq.com <1729372667@qq.com>
2024-07-01 22:40:19 +0800

    (feat) seperate w and p emb

--- model/iTransformer.py
+++ model/iTransformer.py
@@ -14,12 +14,15 @@ class Model(nn.Module):
 
     def __init__(self, configs):
         super(Model, self).__init__()
-        self.seq_len = configs.seq_len
+        self.seq_len_w = configs.seq_len_w
+        self.seq_len_p = configs.seq_len_p
         self.pred_len = configs.pred_len
         self.output_attention = configs.output_attention
         self.use_norm = configs.use_norm
         # Embedding
-        self.enc_embedding = DataEmbedding_inverted(configs.seq_len, configs.d_model, configs.embed, configs.freq,
+        self.enc_embedding_w = DataEmbedding_inverted(configs.seq_len_w, configs.d_model, configs.embed, configs.freq,
+                                                    configs.dropout)
+        self.enc_embedding_p = DataEmbedding_inverted(configs.seq_len_p, configs.d_model, configs.embed, configs.freq,
                                                     configs.dropout)
         self.class_strategy = configs.class_strategy
         # Encoder-only architecture
@@ -38,23 +41,36 @@ class Model(nn.Module):
             norm_layer=torch.nn.LayerNorm(configs.d_model)
         )
         self.projector = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+        
+    def _apply_norm(self, x_enc):
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc = x_enc / stdev
+        return x_enc, means, stdev
+    
+    def _maybe_denorm(self, dec_out, stdev, means):
+        # De-Normalization from Non-stationary Transformer
+        dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+        dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+        return dec_out
 
-    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+    def forecast(self, x_enc_w, x_enc_p, x_mark_enc, x_dec, x_mark_dec):
         if self.use_norm:
-            # Normalization from Non-stationary Transformer
-            means = x_enc.mean(1, keepdim=True).detach()
-            x_enc = x_enc - means
-            stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-            x_enc = x_enc / stdev
-
-        _, _, N = x_enc.shape # B L N
+            x_enc_w, means_w, stdev_w = self._apply_norm(x_enc_w)
+            x_enc_p, means_p, stdev_p = self._apply_norm(x_enc_p)
+        _, _, N = x_enc_w.shape # B L N
         # B: batch_size;    E: d_model; 
         # L: seq_len;       S: pred_len;
         # N: number of variate (tokens), can also includes covariates
 
         # Embedding
         # B L N -> B N E                (B L N -> B L E in the vanilla Transformer)
-        enc_out = self.enc_embedding(x_enc, x_mark_enc) # covariates (e.g timestamp) can be also embedded as tokens
+        # covariates (e.g timestamp) can be also embedded as tokens
+        enc_out_p = self.enc_embedding_p(x_enc_p, x_mark_enc)
+        enc_out_w = self.enc_embedding_w(x_enc_w, x_mark_enc)
+        enc_out = enc_out_p + enc_out_w
         
         # B N E -> B N E                (B L E -> B L E in the vanilla Transformer)
         # the dimensions of embedded time series has been inverted, and then processed by native attn, layernorm and ffn modules
@@ -64,13 +80,11 @@ class Model(nn.Module):
         dec_out = self.projector(enc_out).permute(0, 2, 1)[:, :, :N] # filter the covariates
 
         if self.use_norm:
-            # De-Normalization from Non-stationary Transformer
-            dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
-            dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+            dec_out = self._maybe_denorm(dec_out, stdev_p, means_p)
 
         return dec_out
 
 
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
-        dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+    def forward(self, x_enc_w, x_enc_p, x_mark_enc, x_dec, x_mark_dec, mask=None):
+        dec_out = self.forecast(x_enc_w, x_enc_p, x_mark_enc, x_dec, x_mark_dec)
         return dec_out[:, -self.pred_len:, :]  # [B, L, D]
